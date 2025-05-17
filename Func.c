2d13e31f01c6a59edf14b357abd67a325931faaf
AUnderscoreTam <denykeckes@gmail.com>
2025-05-17 17:50:28 +0200

    polished fundementals

--- Func.c
+++ Func.c
@@ -15,38 +15,48 @@ FILE* createProfile(FILE* pointer,FILE* pP) {
 	}
 
 
-	system("cls");
 	printf("enter your alias\n");
 	scanf("%29[^\n]",temp1.name);
 	while ((getchar()) != '\n');
 	
-	fseek(pointer, 0, SEEK_SET);
-	fread(&id, sizeof(int), 1, pointer);
-	fseek(pointer, ((int)sizeof(int) + (int)sizeof(temp1.name) * id), SEEK_SET);
-	fwrite(&temp1.name, sizeof(temp1.name), 1, pointer);
-	id++;
-	temp1.id = id;
-	fseek(pointer, 0, SEEK_SET);
-	fwrite(&id, sizeof(int), 1, pointer);
-	fseek(pointer, 0, SEEK_SET);
 
 	char name[35];
 	strcpy(name, temp1.name);
-	profilePointer=fopen(strcat(name, txt), "a+");
-	if (profilePointer == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
 
-	fputs(temp1.name,profilePointer);
-	fflush(profilePointer);
-	return profilePointer;
+	if ((fopen(strcat(name, txt), "r")) == NULL)
+	{
+		profilePointer = fopen(name, "a+");
+		if (profilePointer == NULL)
+		{
+			exit(EXIT_FAILURE);
+		}
+
+		fputs(temp1.name, profilePointer);
+		fflush(profilePointer);
+
+		fseek(pointer, 0, SEEK_SET);
+		fread(&id, sizeof(int), 1, pointer);
+		fseek(pointer, ((int)sizeof(int) + (int)sizeof(temp1.name) * id), SEEK_SET);
+		fwrite(&temp1.name, sizeof(temp1.name), 1, pointer);
+		id++;
+		temp1.id = id;
+		fseek(pointer, 0, SEEK_SET);
+		fwrite(&id, sizeof(int), 1, pointer);
+		fseek(pointer, 0, SEEK_SET);
+		return profilePointer;
+	}
+	else
+	{
+		printf("alias already taken");
+		Sleep(3500);
+		createProfile(pointer, pP);
+	}
 }
 
 void showMM(FILE* pP , FILE* pFL) {
 	char profileName[30];
 	int choice = 0;
-	rewind(pP);
+	fseek(pP,0,SEEK_SET);
 	fscanf(pP, "%29[^\n]", profileName);
 
 	printf("Profile selected: %s\n\n",profileName);
@@ -110,9 +120,6 @@ FILE* switchProfile(FILE* pP, FILE* pFL) {
 	{
 		exit(EXIT_FAILURE);
 	}
-	
-	system("cls");
-
 	return profilePointer;
 	
 }
@@ -128,13 +135,12 @@ char* listProfile(FILE* pFL,int n) {
 } // NAUCI KORISTITI RETURNATI STRINGOVE 
 
 void listAllProfiles(FILE* pFL) {
-	system("cls");
 	char name[30];
 	int n = scanId(pFL);
 	
 	for (int i = 0; i < n; i++)
 	{
- 	fread(name, 30, 1, pFL);
+	fread(name, 30, 1, pFL);
 	printf("%d)%s\t", i+1 ,name);
 	}
 }
@@ -151,48 +157,65 @@ void deleteProfile(FILE* pFL, FILE* pP) {
 	int n = scanId(pFL);
 	int choice = 0;
 	listAllProfiles(pFL);
-	char name[35] = { "\0" };
+	char name[30] = {0};
+	char emtpy[30] = {0};
+
 
 	do
 	{
 		scanf("%d", &choice);
 	} while (choice <= 0 || choice > n);
-
+	getchar();
 
 	char namet[35];
 	fseek(pFL, ((int)sizeof(int) + 30 * (choice - 1)), SEEK_SET);
 	fread(namet, 30, 1, pFL);
 	strcat(namet, txt);
 	if ((remove(namet)) != 0){
-	fclose(pP);
-	remove(namet);
+		fclose(pP);
+		remove(namet);
 
-	for (int i = 0; i < (n - choice); i++)
-	{
+		if (n == choice) {
+			fseek(pFL, ((int)sizeof(int) + 30 * (choice-1)), SEEK_SET);
+			fwrite(emtpy, 30, 1, pFL);
+		}
+
+		for (int i = 0; i < (n - choice); i++)
+		{
 		fseek(pFL, ((int)sizeof(int) + 30 * (choice + i)), SEEK_SET);
 		fread(name, 30, 1, pFL);
 		fseek(pFL, ((int)sizeof(int) + 30 * (choice + i)), SEEK_SET);
-		fwrite("\0", 30, 1, pFL);
+		fwrite(emtpy, 30, 1, pFL);
 		fseek(pFL, ((int)sizeof(int) + 30 * ((choice + i) - 1)), SEEK_SET);
 		fwrite(name, 30, 1, pFL);
-	}
+		}
 
-	fseek(pFL, 0, SEEK_SET);
-	n -= 1;
-	fwrite(&n, sizeof(int), 1, pFL);
+		fseek(pFL, 0, SEEK_SET);
+		n -= 1;
+		fwrite(&n, sizeof(int), 1, pFL);
 
-
-	pP = switchProfile(pP, pFL);
-	return;
+		if (n==0)
+		{
+			pP = createProfile(pFL,pP);
+		}
+		else
+		{
+			pP = switchProfile(pP, pFL);
+		}
+		return;
 	};
 	
+	if (n == choice) {
+		fseek(pFL, ((int)sizeof(int) + 30 * (choice-1)), SEEK_SET);
+		fwrite(emtpy, 30, 1, pFL);
+	}
 
 	for (int i = 0; i < (n - choice); i++)
 	{
 		fseek(pFL, ((int)sizeof(int) + 30 * (choice + i)), SEEK_SET);
 		fread(name, 30, 1, pFL);
 		fseek(pFL, ((int)sizeof(int) + 30 * (choice + i)), SEEK_SET);
-		fwrite("\0", 30, 1, pFL);
+		fwrite(emtpy, 30, 1, pFL);
 		fseek(pFL, ((int)sizeof(int) + 30 * ((choice + i) - 1)), SEEK_SET);
 		fwrite(name, 30, 1, pFL);
 	}
